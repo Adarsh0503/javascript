99e739c98caac9ce46364bedd3ea5668da27d5a6
Adarsh Gaurav <100284189+Adarsh0503@users.noreply.github.com>
2023-09-28 13:51:59 +0000

    data types updated

--- basics/conversions.js
+++ basics/conversions.js
@@ -1,12 +1,12 @@
-let score="hirte"
+// let score="hirte"
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let valueInNumber =Number(score); // guarantee ki score ab number ki form me hai
+// let valueInNumber =Number(score); // guarantee ki score ab number ki form me hai
 
-console.log(typeof(valueInNumber));
+// console.log(typeof(valueInNumber));
 
-console.log(valueInNumber)  // Nan (not a number)-?special type
+// console.log(valueInNumber)  // Nan (not a number)-?special type
 
 
 //conversions rules
@@ -15,12 +15,33 @@ console.log(valueInNumber)  // Nan (not a number)-?special type
 // true -> 1
 // false->0
 
-let isloggedIn= 1
-let booleanIsLoggedIn= Boolean(isloggedIn)
+// let isloggedIn= 1
+// let booleanIsLoggedIn= Boolean(isloggedIn)
 
-console.log(typeof(sloggedIn))
-console.log(typeof(booleanIsLoggedIn))
+// console.log(typeof(sloggedIn))
+// console.log(typeof(booleanIsLoggedIn))
 
 // 1-> true  & 0->false
 // ""->false
 // "adarsh"->true
+
+//// ****************operations************/////////////
+
+let value=3
+let negvalue = -value
+
+console.log(negvalue)
+
+let str="adarsh"
+let str2="gaurav"
+let str3= str+str2
+console.log(str3)
+
+console.log(null==0);
+console.log(null<=0);
+
+//===
+//triple check
+console.log("2"===2);
+
+// we should always use same datatypes for comparisons
--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,20 +1,86 @@
-"use strict"; // treat all js code as newer version
+// "use strict"; // treat all js code as newer version
 
-// alert("Hello") // we are using nodejs not browseer
-
-
-  //data types
-  // number -> range till 2^53
-  //bigInt
-  //string->""
-//   bolean=>
-// null -> standalone value
-//undefined -> 
-//symbol-> unique       //mostly used in react
+// // alert("Hello") // we are using nodejs not browseer
 
 
+//   //data types
+//   // number -> range till 2^53
+//   //bigInt
+//   //string->""
+// //   bolean=>
+// // null -> standalone value
+// //undefined -> 
+// //symbol-> unique       //mostly used in react
 
 
-// object 
 
-console.log(typeof null);
+
+// // object 
+
+// console.log(typeof null);
+
+
+
+// // // interview questions important concept//
+// // # primitive ->>  7   //call by value
+// // String 
+// // Number
+// // boolean
+// // null
+// // undefined
+// // Symbol
+// // BigInt
+
+
+
+// //NOTE
+// // JavaScript is a dynamically typed language, which means that data types of variables are determined 
+// // by the value they hold at runtime and can change throughout the program as 
+// // we assign different values to them.
+
+
+
+// // non primitive -->>  call by refrence 
+
+// // arrays
+// // objects
+// // functions
+
+
+
+// const score=100
+// const scoreValue = 100.3
+// const isloggedIn=true;
+// const outTemp=null
+//  let userEmail;  // undefined
+
+//  //symbol
+//  const id= Symbol("123");
+//  const newId=Symbol("123");
+//  console.log(id===newId);
+
+
+
+//  const bigNo= 4748562465765876n
+
+
+
+//  // arrays
+// // objects
+// // functions
+
+// const heroes=["shaktiman","krish","dogi"]
+// let myobj=
+// {
+//   name:"adarsh" ,
+//   age:21
+// }
+
+// const myFunction =function(){
+//   console.log("hi");
+// }
+
+// console.log(typeof(bigNo));
+
+//return type of non primitive data types is mostly object whereas in case of function 
+// it is object function
