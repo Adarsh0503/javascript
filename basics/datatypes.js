9de88b2856be413b6f7f5dc6b19a652f6f37211a
Adarsh Gaurav <100284189+Adarsh0503@users.noreply.github.com>
2023-09-28 09:55:33 +0000

    data types and variables

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello") // we are using nodejs not browseer
+
+
+  //data types
+  // number -> range till 2^53
+  //bigInt
+  //string->""
+//   bolean=>
+// null -> standalone value
+//undefined -> 
+//symbol-> unique       //mostly used in react
+
+
+
+
+// object 
+
+console.log(typeof null);
---
+++ basics/variabes.js
@@ -0,0 +1,24 @@
+const accountId = 144551
+let accountEmail ="asdd@adarsh.com"
+var accountPassword="123221"
+accountCity="Delhi"  // not preffered
+
+// accountId=2  // not allowed
+console.log(accountId);
+accountEmail="wnjwnk@gaurav.com"
+accountPassword="3EWEFWF"
+accountCity="Faridabad"
+
+let accountState; // returns undefined
+
+
+
+/*
+prefered not to use var beacuse of issue 
+in block scope and functional scope
+
+
+*/
+
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState]);
+
