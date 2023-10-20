ae8bde922901c8d9d97f351069c95528327405b2
Adarsh Gaurav <100284189+Adarsh0503@users.noreply.github.com>
2023-10-20 07:04:28 +0000

    till events

---
+++ events/basics.js
@@ -0,0 +1,15 @@
+// js k andar jitna bhi events hote hai wo sequentiallyy hi run hote hai
+// some exception hote hai()
+
+
+// events kisi activity pr invoke hote hai
+
+// dont inject it in html  as its not good
+
+// event listners bhut saare events ko listen kr skata hai (keyboard,mouse,frag and drop , etc)
+// event listners details
+// document.getElementById("idname").addEventListener('clcik',function(){
+
+// }, false )
+
+// yeha pr false is 3rd parameter which is by default 
